a27b3a666a8b8d2b5a44a38e5cc4f910a078af70
Mostafa Ali Mansour <tifa64@users.noreply.github.com>
2021-10-03 19:58:59 +0200

    Supporting Linux control groups

--- main.go
+++ main.go
@@ -57,6 +57,16 @@ func child() {
 	must(syscall.Unmount("thing", 0))
 }
 
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "liz"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+}
+
 func must (err error) {
   if err != nill
     panic(err)
