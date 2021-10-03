be482660591638b65bd0e10fe37738e2681cfafc
Mostafa Ali Mansour <tifa64@users.noreply.github.com>
2021-10-03 19:57:45 +0200

    Creating child method for child processes

--- main.go
+++ main.go
@@ -30,6 +30,28 @@ func run() {
 	must(cmd.Run())
 }
 
+func child() {
+	fmt.Printf("Running %v \n", os.Args[2:])
+
+	cg()
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Chroot("/home/mostafa/workspace"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
+	must(syscall.Unmount("thing", 0))
+}
+
 func must (err error) {
   if err != nill
     panic(err)
